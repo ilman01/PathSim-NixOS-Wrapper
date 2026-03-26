8220ce53a06bdec44f094a416885ab81c63328f3
ilman01 <ilmanzidni2006@gmail.com>
2026-03-26 16:56:27 +0700

    removed java_home

--- shell.nix
+++ shell.nix
@@ -18,7 +18,6 @@ pkgs.mkShell {
 
   shellHook = ''
     export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath runtimeLibs}
-    export JAVA_HOME=${pkgs.jdk17}
     
     # Needed sometimes for GTK apps (fixes weird runtime issues)
     export GSETTINGS_SCHEMA_DIR=${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas
